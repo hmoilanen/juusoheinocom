1e194c9fe01a8806a3da5338d90aab8de368c697
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-08-20 12:41:42 +0300

    cleaned up homeProjects

--- src/components/homeProjects.vue
+++ src/components/homeProjects.vue
@@ -1,58 +1,46 @@
 <template>
   <div class="home-projects">
     <app-content-wrapper>
-      <editable-content path="home.projects" #default="{ content }">
-				<base-flex center="y">
-        	<app-title class="gsap--home-projects--title">{{ content[`title-${$app.locale()}`] }}</app-title>
+      
+			<editable-content path="home.projects" #default="{ content }">
+				<base-flex class="gsap--home-projects--title" center="y">
+        	<app-title>{{ content[`title-${$app.locale()}`] }}</app-title>
 					<base-icon
 						class="shuffle"
 						@click="newRandomProject"
 						:only-stroke="true"
 					>shuffle</base-icon>
 				</base-flex>
+			</editable-content>
 
-				<div class="gsap--home-projects--project">
-					<base-title
-						:size="7"
-						:scaling="1"
-						:m-t="10"
-					>{{ randomProject[`title-${$app.locale()}`] }}</base-title>
-					<base-title
-						:size="4"
-						:scaling="1"
-					>{{ randomProject[`type-${$app.locale()}`] }}, {{ randomProject.year }}</base-title>
-				</div>
-				<!-- <base-flex class="gsap--home-projects--title">
-					<base-link :to="{ name: 'projects' }">
-						<base-flex>
-							<base-icon :only-stroke="true">redirect</base-icon>
-							<base-title :size="6" :m-l="4">to projects</base-title>
-						</base-flex>
-					</base-link>
-					<base-flex @click="newRandomProject" class="shuffle">
-						<base-icon :only-stroke="true">shuffle</base-icon>
-						<base-title :size="6" :m-l="4">shuffle</base-title>
+			<!-- PÄÄTÄ MITÄ TÄLLE TEHDÄÄN KUN LAYOUT ON VALMIS -> TULEEKO KÄYTTÖÖN VAI MENEEKÖ ROSKIIN ?! -->
+			<!-- <base-flex class="gsap--home-projects--title">
+				<base-link :to="{ name: 'projects' }">
+					<base-flex>
+						<base-icon :only-stroke="true">redirect</base-icon>
+						<base-title :size="6" :m-l="4">to projects</base-title>
 					</base-flex>
-				</base-flex> -->
+				</base-link>
+				<base-flex @click="newRandomProject" class="shuffle">
+					<base-icon :only-stroke="true">shuffle</base-icon>
+					<base-title :size="6" :m-l="4">shuffle</base-title>
+				</base-flex>
+			</base-flex> -->
 
-        <!-- <base-link :to="{ name: 'projects' }">
-          <base-flex class="gsap--home-projects--title" center="y">
-            <base-title :size="7">{{ content[`link-${$app.locale()}`] }}</base-title>
-            <base-icon class="redirect" :only-stroke="true">redirect</base-icon>
-          </base-flex>
-        </base-link> -->
-      </editable-content>
+			<div class="gsap--home-projects--project">
+				<base-title
+					:size="7"
+					:scaling="1"
+					:m-t="10"
+				>{{ randomProject[`title-${$app.locale()}`] }}</base-title>
+				<base-title
+					:size="4"
+					:scaling="1"
+				>{{ randomProject[`type-${$app.locale()}`] }}, {{ randomProject.year }}</base-title>
+			</div>
 
 			<base-wrapper class="gsap--home-projects--project" maxWidth="medium" :m-t="20">
 				<content-carousel :key="randomProject.id">
-					<!-- <div
-						v-for="(image, index) in randomProject.images"
-						:key="image + index"
-						class="image"
-						@click="goToCurrentProject"
-					>
-						<base-bg :source="imageSource(image)" fit="contain"></base-bg>
-					</div> -->
 					<content-carousel-item
 						v-for="(image, index) in randomProject.images"
 						:key="index"
@@ -61,43 +49,8 @@
 						@click="goToCurrentProject"
 					></content-carousel-item>
 				</content-carousel>
-
-				<!-- <base-flex :m-t="42" center="x">
-					<base-link :to="{ name: 'projects' }" :m-l="-5">
-						<base-flex>
-							<base-icon :only-stroke="true">redirect</base-icon>
-							<base-title :size="6" :m-l="4">to projects</base-title>
-						</base-flex>
-					</base-link>
-					<base-flex @click="newRandomProject" class="shuffle">
-						<base-icon :only-stroke="true">shuffle</base-icon>
-						<base-title :size="6" :m-l="4">shuffle</base-title>
-					</base-flex>
-				</base-flex> -->
 			</base-wrapper>
 
-			<!-- <base-icon class="go-to-projects" :size="34">goto</base-icon> -->
-      
-      <!-- <div class="grid" :style="styling">
-        <div
-          class="card"
-          v-for="(project, key) in projects"
-          :key="key"
-        >
-          <base-link :to="{ name: 'project', params: { id: key } }">
-            <div class="bg">
-              <base-bg :source="bgSource(key, project.bg)" posX="center" posY="top"></base-bg>
-            </div>
-          </base-link>
-          <div class="info">
-            <base-title :size="8">{{ project['title-' + $app.locale()] }}</base-title>
-            <base-text>{{ project.year }}</base-text>
-          </div>
-          <base-link class="link" :to="{ name: 'project', params: { id: key } }">
-            <base-text>more &#8594;</base-text>
-          </base-link>
-        </div>
-      </div> -->
     </app-content-wrapper>
   </div>
 </template>
@@ -136,17 +89,10 @@ export default {
 	},
 
   mounted() {
-		/* for (let i = 3; i < 10; i++) {
-			const index = randomIntegerFromInterval(0, i)
-			console.log(index);
-		} */
-
 		const tl = gsap.timeline({
 			scrollTrigger: {
 				trigger: '.home-projects',
 				start: 'top center',
-				//markers: true,
-				//toggleActions: 'restart reset reset reset'
 			}
 		})
 
@@ -168,51 +114,20 @@ export default {
 	},
 
   computed: {
-    projects() {
-      if (!this.$app.isLoading()) {
-        const { main, ...projects } = this.$store.state.content.projects        
-        return projects
-      } else return null
-		},
-		
     randomProject() {
-			//const index = randomIntegerFromInterval(0, keys.length - 1)
-			//const index = randomIntegerFromInterval(0, 2) //DUMMY!
-
-			const keys = Object.keys(this.projects)
+			const { main, ...projects } = this.$store.state.content.projects
+			const keys = Object.keys(projects)
 			const project = Object.assign(
 				{},
-				this.projects[keys[this.randomProjectIndex]],
+				projects[keys[this.randomProjectIndex]],
 				{ id: keys[this.randomProjectIndex] }
 			)
 
 			return project
-    },
-
-    styling() {
-      // HUOM! LAITA LOPUKSI GAP SYNKKAAMAAN base-view:N PADDINGIN KOON KANSSA!!!
-      // HUOM! LAITA LOPUKSI GAP SYNKKAAMAAN base-view:N PADDINGIN KOON KANSSA!!!
-      // HUOM! LAITA LOPUKSI GAP SYNKKAAMAAN base-view:N PADDINGIN KOON KANSSA!!!
-      // HUOM! NYT SÄÄDÖT DUMMYJA
-      let windowWidth = this.$store.state.ui.window.width
-      let breakpoint = this.$store.getters['ui/GET_BREAKPOINT']
-      let gap = 1 // = '1rem'
-      
-      if (windowWidth > 900) { gap = 1.8 }
-      else if (windowWidth > 1000) { gap = 2.8 }
-
-      return {
-        gridGap: gap + 'rem'
-      }
     }
   },
 
-  methods: {
-    /* bgSource(key, imageName) {
-      let imageURL = this.$store.getters['app/GET_URL'].imageURL
-      return `${imageURL}projects/${key}/${imageName}`
-		}, */
-		
+  methods: {		
 		imageSource(image) {
       const imageURL = this.$store.getters['app/GET_URL'].imageURL
       return `${imageURL}projects/${this.randomProject.id}/${image}`
@@ -224,6 +139,8 @@ export default {
 
 		newRandomProject() {
 			let newIndex = randomIntegerFromInterval(0, 2) //DUMMY!
+			//TUDUU: TÄMÄ TULEEE TILALLE KUN KUVAT LAITETTU KAANTAAN:
+			//let newIndex = randomIntegerFromInterval(0, keys.length - 1)
 
 			while (newIndex === this.randomProjectIndex) {
 				newIndex = randomIntegerFromInterval(0, 2)
@@ -242,66 +159,16 @@ $home-projects--color-hl: $app-color--hl;
 	min-height: 100vh;
 	display: flex;
 	flex-direction: column;
-  //align-items: center;
 	justify-content: center;
-	//background: rgba(0, 0, 0, 0.01);//POISTUU!!!
 
-	.app-content-wrapper {
-		width: 100%;
-	}
-
+	.app-content-wrapper { width: 100%; }
 	.app-title { margin-bottom: 0; }
-
+	.shuffle { @extend %clickable; }
   .redirect {
     margin-left: 0.5rem;
     margin-top: -0.1rem;
     @extend %clickable;
     color: $home-projects--color-hl;
-  }
-  .grid {
-    margin-top: 2rem;
-    display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-    // grid-gap, see: this.styling
-    transition: grid-gap 0.3s ease;
-  }
-  .card {
-    .bg {
-      position: relative;
-      height: 0;
-      padding-bottom: 100%;
-    }
-    .info {
-      margin-top: 0.8rem;
-      margin-bottom: 0.2rem;
-      display: flex;
-      align-items: baseline;
-      justify-content: space-between;
-    }
-  }
-  //.base-link:hover .base-text { color: $home-projects--color-hl; }
-  .base-link:hover {
-    &, & .base-text { color: $home-projects--color-hl; }
 	}
-
-	/* .image {
-		position: relative;
-    height: 0;
-		padding-bottom: 56.25%;
-		@extend %clickable;
-		&:hover { opacity: 0.8; }
-		//max-width: calc(100vw - (2 * 1rem));
-	} */
-
-	.shuffle {
-		@extend %clickable;
-		//margin-left: 1.5rem; 
-	}
-	
-	/* .go-to-projects {
-		@extend %icon--only-stroke;
-		stroke-width: 2px;
-		//stroke: black;
-	} */
 }
 </style>
