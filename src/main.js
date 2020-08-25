fa6ca2b259ba041b10a859d163e004ae5e2936a6
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-08-25 17:22:52 +0300

    deleted some selfmade unused text files

--- src/main.js
+++ src/main.js
@@ -3,14 +3,14 @@ import App from '@/App.vue'
 import router from '@/router/index'
 import store from '@/store/index'
 import api from "@/api"
-import '@/components/base/_globals' // Global base components
+import '@/components/base/_globals'
 import '@/registerServiceWorker'
 
 Vue.use(api)
 
-Vue.config.productionTip = false
-Vue.config.performance = process.env.NODE_ENV !== 'production' ? true : false
 // For component performance tracking, see: https://vuedose.tips/tips/measure-runtime-performance-in-vue-js-apps/
+Vue.config.performance = process.env.NODE_ENV !== 'production' ? true : false
+Vue.config.productionTip = false
 
 new Vue({
   router,
