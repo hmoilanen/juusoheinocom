71fc34507dce9ebb9291dc05fc3b1dd454b56f5b
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-08-25 17:00:18 +0300

    cleaned whole /styles

--- src/App.vue
+++ src/App.vue
@@ -35,38 +35,5 @@ export default {
 </script>
 
 <style lang="scss">
-// TODO!: TEE ERILLINEN CSS-RESET -FILU JOKA IMPORTATAAN TÄHÄN (= 'cssReset.scss') ?!?!?!
-// KOITIN MUTTA TEKI JOSTAIN SYYSTÄ ISON MÄÄRÄN DUPLIKAATTITYYLEJÄ INSPECTIIN -> SELVITÄ
-
-*,
-*::before,
-*::after { box-sizing: border-box; }
-//VAI PITÄISKÖ TÄHÄN LAITTAA SUORAAN MYÖS MARGNI/PADDING: 0 ???
-
-body {
-  margin: 0;
-  padding: 0;
-  background-color: $app-color--bg;
-  font-size: 1rem;
-  line-height: 1.35em;
-  font-family: $app-font--base;
-  color: $app-color--text-base;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-	font-variant-ligatures: none;
-}
-
-h1, h2, h3, h4, h5, h6 { margin: 0; }
-
-ol, ul, li {
-  margin: 0;
-  padding: 0;
-}
-
-p {
-  margin: 0;
-  padding: 0;
-}
-
-canvas { display: block; }
+@import "./styles/cssReset.scss"
 </style>
