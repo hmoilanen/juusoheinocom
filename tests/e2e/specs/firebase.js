d0c3ffff6eb264a9a78c6a50bbaad603b59330a8
hmoilanen <juuso.ville.henrikki@gmail.com>
2020-09-17 17:18:56 +0300

    cypress tests for firebase improved

--- tests/e2e/specs/firebase.js
+++ tests/e2e/specs/firebase.js
@@ -1,23 +1,28 @@
 // For testing the connection and functionality of Firebase
 
 describe('Generic Firebase Tests', () => {
+	const mockFileName = 'CY-test'
+
   it('Adds document to "tests" collection of Firestore', () => {
 		cy.callFirestore('get', 'tests')
 			.then(resBefore => {
-				cy.log('response:', resBefore.length)
+				const resBeforeLength = resBefore ? resBefore.length : 0
 
-				cy.callFirestore('add', 'tests', { some: 'value' })
+				cy.log('response:', resBeforeLength)
+
+				cy.callFirestore('set', `tests/${mockFileName}`, { some: 'value' })
 
 				cy.callFirestore('get', 'tests')
 					.then(resAfter => {
 						cy.log('response:', resAfter.length)
 
-						expect(resAfter.length).to.be.greaterThan(resBefore.length)
+						expect(resAfter.length).to.be.greaterThan(resBeforeLength)
 					})
 			})
 	})
 
-  /* it('Deletes a document from "tests" collection of Firestore', () => {
-    cy.callFirestore('add', 'tests', { some: 'value' })
-	}) */
+  it('Deletes a document from "tests" collection of Firestore', () => {
+		const opts = { recursive: true }
+    cy.callFirestore('delete', `tests/${mockFileName}`)
+	})
 })
